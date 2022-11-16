f1a55a92af3c27faa277fe007a16b3a620ec5ac4
Shafiujjaman-shafi <jcmshafi96@gmail.com>
2022-11-17 02:08:58 +0600

    working with mainActivity and set task

--- app/src/main/java/com/example/todo/Adapter/ToDoAdapter.java
+++ app/src/main/java/com/example/todo/Adapter/ToDoAdapter.java
@@ -22,11 +22,32 @@ public class ToDoAdapter extends RecyclerView.Adapter<ToDoAdapter.ViewHolder> {
 
         this.activity = activity;
     }
+
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View itemView = LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.task_layout, parent, false);
         return new ViewHolder(itemView);
     }
+
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        ToDoModel item = todoList.get(position);
+        holder.task.setText(item.getTask());
+        holder.task.setChecked(toBoolean(item.getStatus()));
+    }
+
+    private boolean toBoolean(int n) {
+        return n != 0;
+    }
+
+    public int getItemCount() {
+        return todoList.size();
+    }
+    public void setTasks(List<ToDoModel> todoList){
+        this.todoList = todoList;
+        notifyDataSetChanged();
+
+    }
+
     public static class ViewHolder extends RecyclerView.ViewHolder {
         CheckBox task;
 
@@ -36,26 +57,6 @@ public class ToDoAdapter extends RecyclerView.Adapter<ToDoAdapter.ViewHolder> {
 
         }
     }
-    public void onBindViewHolder(ViewHolder holder, int position) {
-        ToDoModel item = todoList.get(position);
-        holder.task.setText(item.getTask());
-        holder.task.setChecked(toBoolean(item.getStatus()));
-    }
-    private boolean toBoolean(int n) {
-        return n != 0;
-    }
-
-    public int getItemCount() {
-        return todoList.size();
-    }
-
-
-
-
-
-
-
-
 
 
 }
